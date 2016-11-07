f0a684c33487de47191af658b3a32ea274dedf74
xcr <xecor2k@hotmail.com>
2016-11-07 06:04:17 +0100

    Added settings for more modes

--- project3/src/main/java/Settings.java
+++ project3/src/main/java/Settings.java
@@ -13,6 +13,27 @@ final class Settings {
             som.expRadiusDecay = 0.1;          // 0.01, 0.003 for < 500 cities
             som.nodesPerCity = 8;               // 8
         }
+        else if (learningDecayType==EXP && radiusDecayType==LIN) {
+            som.initLearningRate = 0.4;
+            som.expLearningDecay = 0.001;
+            som.initRadiusNormalised = 1.0/2;
+            som.targetLinRadius = 0;
+            som.nodesPerCity = 8;
+        }
+        else if (learningDecayType==EXP && radiusDecayType==STATIC) {
+            som.initLearningRate = 0.4;
+            som.expLearningDecay = 0.001;
+            som.initRadiusNormalised = 1.0/20;
+            som.nodesPerCity = 8;
+        }
+
+        else if (learningDecayType==LIN && radiusDecayType==EXP) {
+            som.initLearningRate = 0.6;         //  0.1 for small, 0.4 for cities>300
+            som.targetLinLearningRate = 0.3;    // 0.01 for small, 0.1 for cities>300
+            som.initRadiusNormalised = 1.0/2;   // 1.0/2
+            som.expRadiusDecay = 0.1;          // 0.01, 0.003 for < 500 cities
+            som.nodesPerCity = 8;
+        }
 
         else if (learningDecayType==LIN && radiusDecayType==LIN) {
             som.initLearningRate = 0.6;         //  0.1 for small, 0.4 for cities>300
@@ -21,10 +42,29 @@ final class Settings {
             som.targetLinRadius = 0;
             som.nodesPerCity = 8;
         }
+        else if (learningDecayType==LIN && radiusDecayType==STATIC) {
+            som.initLearningRate = 0.6;         //  0.1 for small, 0.4 for cities>300
+            som.targetLinLearningRate = 0.3;    // 0.01 for small, 0.1 for cities>300
+            som.initRadiusNormalised = 1.0/20;
+            som.nodesPerCity = 8;
+        }
+
+        else if (learningDecayType==STATIC && radiusDecayType==EXP) {
+            som.initLearningRate = 0.2;
+            som.initRadiusNormalised = 1.0/5; // 1.0/5 on small, 1.0/10 on > 50
+            som.expRadiusDecay = 0.1;          // 0.01, 0.003 for < 500 cities
+            som.nodesPerCity = 8;
+        }
+        else if (learningDecayType==STATIC && radiusDecayType==LIN) {
+            som.initLearningRate = 0.2;
+            som.initRadiusNormalised = 1.0/5; // 1.0/5 on small, 1.0/10 on > 50
+            som.targetLinRadius = 0;
+            som.nodesPerCity = 8;
+        }
 
         else if (learningDecayType==STATIC && radiusDecayType==STATIC) {
             som.initLearningRate = 0.2;
-            som.initRadiusNormalised = 1.0/100; // 1.0/5 on small, 1.0/10 on > 50
+            som.initRadiusNormalised = 1.0/5; // 1.0/5 on small, 1.0/10 on > 50
             som.nodesPerCity = 50;
         }
 
